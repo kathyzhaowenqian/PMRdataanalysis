cbde9bda19f42d2b654f952730d894db6299248d
Kathyzhaowenqian <wzywdq@163.com>
2023-04-11 19:04:48 +0800

    0411-1.

--- PMRdataanalysisV2/production_settings.py
+++ PMRdataanalysisV2/production_settings.py
@@ -160,7 +160,7 @@ AUTH_USER_MODEL = 'Marketing_Research.UserInfo'
 
 SIMPLEUI_ANALYSIS = False
 
-SIMPLEUI_HOME_PAGE ='http://139.224.61.6:8088/login/'
+SIMPLEUI_HOME_PAGE ='http://data.bio-merry.com:8088/login/'
 SIMPLEUI_HOME_TITLE = 'Superset BI 数据看板'
 SIMPLEUI_HOME_ICON = 'fa-sharp fa-solid fa-chart-simple'
 
