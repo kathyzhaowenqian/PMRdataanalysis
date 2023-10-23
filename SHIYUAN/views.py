f13a778d0b36f1edc4a62c22d2edf84c3158ef37
kathyzwq <wzywdq@163.com>
2023-10-23 20:18:33 +0800

    1023-7.

--- SHIYUAN/views.py
+++ SHIYUAN/views.py
@@ -15,28 +15,31 @@ import io
 from SHIYUAN.tools.CalculateAPI import *
 import os
 class Upload(View):
+
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+        self.Upload_File = os.path.join(os.path.dirname(__file__), 'file/sy', 'ShiYuan.xlsx')
+
     def get(self,request):
-        # if os.path.exists('十院明细.xlsx'):  # 判断文件是否存在
-        #     os.remove('十院明细.xlsx')       # 删除文件
-        # if os.path.exists('ShiYuan.xlsx'):  # 判断文件是否存在
-        #     os.remove('ShiYuan.xlsx')       # 删除文件
+        print('os.path.dirname(__file__),',os.path.dirname(__file__))
+              
+        if os.path.exists(self.Upload_File):  # 判断文件是否存在
+            os.remove(self.Upload_File)       # 删除文件
+
         return render(request,'SY/upload.html')
     
     def post(self,request):
         if  request.FILES['excelFile']:
-
+            
             excel_file = request.FILES['excelFile']
             print('看一下格式excel_file',excel_file)
-            # order_data = pd.read_excel(excel_file, sheet_name='订单')
-            # # 处理数据调整逻辑
-            # # ...
-            # # 存储调整后的数据到新的Excel文件
-            # new_excel_file = 'ShiYuan.xlsx'
-            # order_data.to_excel(new_excel_file, index=False)
-            file_name = 'ShiYuan.xlsx'
+            file_name = self.Upload_File
+            # file_name = '/file/sy/ShiYuan.xlsx'
             if os.path.exists(file_name):  # 判断文件是否存在
                 os.remove(file_name)       # 删除文件
+
             sheet_name_list = ['订单统计','订单明细(上传的)','直送入库和康意路出入库明细(上传的)','康意路库存','康意路库存(带批次)','医院端的库存和领用汇总','医院端的库存和领用汇总(带批次)','领用明细(上传的)']
+            
             writer = pd.ExcelWriter(file_name)
             result_list=SHIYUAN(excel_file)
 
@@ -44,7 +47,6 @@ class Upload(View):
                 result_list[i]=result_list[i].style.set_properties(**{'text-align': 'center'}) ## 使excel表格中的数据居中对齐
                 result_list[i].to_excel(writer, sheet_name=sheet_name_list[i],index=False)
                 worksheet = writer.sheets[sheet_name_list[i]]
-            #     worksheet.set_column('A:B',16) ## 设置excel表格列宽为16
             writer.close()
 
             return HttpResponseRedirect('/SHIYUAN/downloads')
@@ -53,11 +55,15 @@ class Upload(View):
 
 
 class Download(View):
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+        self.Upload_File = os.path.join(os.path.dirname(__file__), 'file/sy', 'ShiYuan.xlsx')
+
     def get(self,request):
-        file_path = 'ShiYuan.xlsx'
+        file_path = self.Upload_File
         with open(file_path, 'rb') as f:
             response = HttpResponse(f.read())
             
-            response['Content-Disposition'] = 'attachment; filename=ShiYuan.xlsx'
+            response['Content-Disposition'] = 'attachment; filename=ShiYuanCalculate.xlsx'
             response['Content-Type'] = 'application/vnd.ms-excel'
             return response
